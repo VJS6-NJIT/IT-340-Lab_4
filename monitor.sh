e5a76b258a876c3022b5c572d3095c1f7a57f82d
VJS6-NJIT <vjs6@njit.edu>
2026-02-20 12:12:58 -0500

    Updated Title Report to SYSTEM REPORT (Memory) in disk-space branch

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 #Log the date and memory usage
 
-echo "Memory Log - $(date)" >> system_log.txt
-free -h | grep Mem >> system_log.txt
-echo "----------------------------------" >> system_log.txt
+echo "SYSTEM REPORT (Memory) - $(date)" >>/home/vincent-steiner/Lab_4/system_log.txt
+/usr/bin/free -h | user/bin/grep Mem >> /home/vincent-steiner/Lab_4/system_log.txt
+echo "----------------------------------" >> /home/vincent-steiner/Lab_4/system_log.txt
